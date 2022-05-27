06bcc3394d366aa17194efd3f4ea4be64efe9946
Leonardo Dos Santos Lopes <leos_lopes@hotmail.com>
2022-05-26 23:44:06 -0300

    Revert "commit for"
    This reverts commit 529d2bb60ee46faf30793717396e9ef99b3773a1.

--- Estruturas de Repetição - For/Exercicio01.js
+++
@@ -1,5 +0,0 @@
-//01 – Faça um algoritmo que escreva 5 vezes a mensagem: “Olá, eu sou uma estrutura de repetição”.
-
-for (let i = 0; i < 5; i++) {
-	console.log("Olá, sou uma estrutura de repetição");
-}
